c22f12f3409074b8de3ff860c59ecf5e061f4218
birkh <birkheuere@gmail.com>
2023-02-03 23:33:49 -0300

    Criado o corpo das funções minimax.

--- advsearch/your_agent/agent.py
+++ advsearch/your_agent/agent.py
@@ -9,6 +9,9 @@ from ..othello.gamestate import GameState
 # Nao esqueca de renomear 'your_agent' com o nome
 # do seu agente.
 
+inf_positivo = float('inf')
+inf_negativo = float('-inf')
+
 
 def make_move(state: GameState) -> Tuple[int, int]:
     """
@@ -16,8 +19,55 @@ def make_move(state: GameState) -> Tuple[int, int]:
     :param state: state to make the move
     :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
     """
-    # o codigo abaixo apenas retorna um movimento aleatorio valido para
-    # a primeira jogada com as pretas.
-    # Remova-o e coloque a sua implementacao da poda alpha-beta
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+    # Lembrar: essa função deve retornar uma JOGADA (x,y)
+
+    valor_jogada = MAX_joga(state, inf_positivo, inf_negativo)
+    return valor_jogada
+
+
+def MAX_joga(state: GameState, alfa, beta):
+    if criterio_de_parada(): # criterio de parada é uma função a ser criada que deve limitar a profundidade da busca, seja apenas estipulando um limite de profundidade ou usando uma tecnica mais sofisticada
+        return avalia_estado()   #  criar função que avalia um estado
+    else:
+        # inicializa v com -infinito
+        v = inf_negativo
+
+        # itera a lista de possiveis movimentos no estado atual do tabuleiro
+        # a lista seria do tipo [(1, 1), (2, 3), (3, 6)]
+        # para cada jogada dentro dessa lista, o minimax faz seus calculos
+        for movimento in list(state.legal_moves()):
+
+            # cria um estado cujo movimento atual causaria 
+            estado = state.next_state(movimento) # talvez seja mais apropriado utilizar state.copy() ao inves de state.next_state(movimento)
+
+            # algoritmo segue conforme os slides da poda alfa-beta
+            v = max(v, MIN_joga(estado, alfa, beta))
+            alfa = max(alfa, v)
+            if alfa >= beta:
+                break
+        return v # poderiamos também retornar alfa ao inves de v, não afetaria o funcionamento
+
+
+
+def MIN_joga(state: GameState, alfa, beta):
+    if criterio_de_parada(): # criterio de parada é uma função a ser criada que deve limitar a profundidade da busca, seja apenas estipulando um limite de profundidade ou usando uma tecnica mais sofisticada
+        return avalia_estado()   #  criar função que avalia um estado
+    else:
+        v = inf_positivo
+        for movimento in state.legal_moves():
+            estado = state.next_state(movimento)
+            v = min(v, MAX_joga(estado, alfa, beta))
+            beta = min(beta, v)
+            if beta <= alfa:
+                break
+        return v # poderiamos também retornar beta ao inves de v, não afetaria o funcionamento
+
+
+def avalia_estado():
+
+def criterio_de_parada():
+
+
+
+
 
