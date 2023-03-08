0c3aa6e4b3cef05a4b3096565a76c8db48753971
edbir <85709577+edbir@users.noreply.github.com>
2023-03-08 18:29:11 -0300

    Update agent.py

--- advsearch/your_agent/agent.py
+++ advsearch/your_agent/agent.py
@@ -12,7 +12,7 @@ from ..othello.gamestate import GameState
 inf_positivo = float('inf')
 inf_negativo = float('-inf')
 
-profundidade = 0
+
 
 def make_move(state: GameState) -> Tuple[int, int]:
     """
@@ -21,8 +21,8 @@ def make_move(state: GameState) -> Tuple[int, int]:
     :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
     """
     # Lembrar: essa função deve retornar uma JOGADA (x,y)
-    #estado = state.copy()
-    #valor_jogada = MAX_joga(estado, inf_positivo, inf_negativo)
+    # estado = state.copy()
+    # valor_jogada = MAX_joga(estado, inf_positivo, inf_negativo)
     prof = 0
 
     jogadas = list(state.legal_moves())
@@ -31,8 +31,11 @@ def make_move(state: GameState) -> Tuple[int, int]:
     jogada_atual = (0, 0)
 
     for jogada in jogadas:
-        estado = state.next_state(jogada).copy()
-        valor_jogada = MAX_joga(estado, inf_positivo, inf_negativo, prof)
+        estado = state.copy().next_state(jogada)
+        if state.player == 'B':
+            valor_jogada = MAX_joga(estado, inf_negativo, inf_positivo, prof)
+        else:
+            valor_jogada = MIN_joga(estado, inf_negativo, inf_positivo, prof)
         if melhor_jogada < valor_jogada:
             melhor_jogada = valor_jogada
             jogada_atual = jogada
@@ -40,51 +43,55 @@ def make_move(state: GameState) -> Tuple[int, int]:
     return jogada_atual
 
 
-def MAX_joga(state: GameState, alfa, beta, prof):
-    if  prof == 5: # criterio de parada é uma função a ser criada que deve limitar a profundidade da busca, seja apenas estipulando um limite de profundidade ou usando uma tecnica mais sofisticada
-        prof = 0 # reseta a profundidade para 0
-        return contas_pecas_pretas(state)   #  criar função que avalia um estado
+def MAX_joga(state: GameState, alfa, beta, profundidade):
+    if profundidade == 3 or state.is_terminal():  # criterio de parada é uma função a ser criada que deve limitar a profundidade da busca, seja apenas estipulando um limite de profundidade ou usando uma tecnica mais sofisticada
+        profundidade = 0  # reseta a profundidade para 0
+        return contas_pecas_pretas(state)  # criar função que avalia um estado
     else:
         # inicializa v com -infinito
         v = inf_negativo
+        if state.legal_moves() == None:
+            return contas_pecas_pretas(state)
+        else:
+            # itera a lista de possiveis movimentos no estado atual do tabuleiro
+            # a lista seria do tipo [(1, 1), (2, 3), (3, 6)]
+            # para cada jogada dentro dessa lista, o minimax faz seus calculos
+            for movimento in list(state.legal_moves()):
+                # cria um estado cujo movimento atual causaria
+                
+                estado = state.next_state(movimento)  # talvez seja mais apropriado utilizar state.copy() ao inves de state.next_state(movimento)
+                
+                # algoritmo segue conforme os slides da poda alfa-beta
+                v = max(v, MIN_joga(estado, alfa, beta, profundidade+1))
+                alfa = max(alfa, v)
+                if alfa >= beta:
+                    break
+            
 
-        # itera a lista de possiveis movimentos no estado atual do tabuleiro
-        # a lista seria do tipo [(1, 1), (2, 3), (3, 6)]
-        # para cada jogada dentro dessa lista, o minimax faz seus calculos
-        for movimento in list(state.legal_moves()):
-
-            # cria um estado cujo movimento atual causaria 
-            estado = state.next_state(movimento) # talvez seja mais apropriado utilizar state.copy() ao inves de state.next_state(movimento)
-
-            # algoritmo segue conforme os slides da poda alfa-beta
-            v = max(v, MIN_joga(estado, alfa, beta, prof))
-            alfa = max(alfa, v)
-            if alfa >= beta:
-                break
-        prof += 1
         return v # poderiamos também retornar alfa ao inves de v, não afetaria o funcionamento
 
 
-
-def MIN_joga(state: GameState, alfa, beta, prof):
-    if prof == 5: # criterio de parada é uma função a ser criada que deve limitar a profundidade da busca, seja apenas estipulando um limite de profundidade ou usando uma tecnica mais sofisticada
-        prof = 0  # reseta a profundidade para 0
-        return contas_pecas_brancas(state)   #  criar função que avalia um estado
+def MIN_joga(state: GameState, alfa, beta, profundidade):
+    if profundidade == 3 or state.is_terminal():  # criterio de parada é uma função a ser criada que deve limitar a profundidade da busca, seja apenas estipulando um limite de profundidade ou usando uma tecnica mais sofisticada
+        profundidade = 0  # reseta a profundidade para 0
+        return contas_pecas_brancas(state)  # criar função que avalia um estado
     else:
         v = inf_positivo
-        for movimento in state.legal_moves():
-            estado = state.next_state(movimento)
-            v = min(v, MAX_joga(estado, alfa, beta, prof))          
-            beta = min(beta, v)
-            if beta <= alfa:
-                break
-        prof += 1
-        return v # poderiamos também retornar beta ao inves de v, não afetaria o funcionamento
-
+        if state.legal_moves() == None:
+            return contas_pecas_brancas(state)
+        else:
+            for movimento in state.legal_moves():
+               
+                estado = state.next_state(movimento)
+                v = min(v, MAX_joga(estado, alfa, beta, profundidade+1))
+                beta = min(beta, v)
+                if beta <= alfa:
+                    break
+        
+        return v  # poderiamos também retornar beta ao inves de v, não afetaria o funcionamento
 
 
 def contas_pecas_pretas(state: GameState):
-
     # Cria variavel do tabuleiro usando get_board do GameState
     board = state.get_board()
 
@@ -95,7 +102,6 @@ def contas_pecas_pretas(state: GameState):
 
 
 def contas_pecas_brancas(state: GameState):
-
     # Cria variavel do tabuleiro usando get_board do GameState
     board = state.get_board()
 
